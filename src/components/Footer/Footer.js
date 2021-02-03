9f609fcc8ef60d02991083de4856c8ed49ab4d12
Guillem <guillem.dardill@gmail.com>
2021-02-03 10:34:01 +0100

    footer CSS added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,16 @@ import React from 'react';
 import './Footer.scss';
 
 const Footer = () => {
-  return <div className='footer'>Footer</div>;
+  return (
+    <div className='footer'>
+      <p>
+        Fanimals est un organisme à but non lucratif qui ne fonctionne qu’avec
+        des fonds privés et ne bénéficie d’aucune subvention publique. Nous
+        avons besoin de vous !
+      </p>
+      <button type='button'>Faire un don</button>
+    </div>
+  );
 };
 
 export default Footer;
