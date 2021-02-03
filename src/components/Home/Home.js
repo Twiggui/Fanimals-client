7dffda85dacaca0205c32b8c7a67493209d9cc5e
Guillem <guillem.dardill@gmail.com>
2021-02-03 11:06:59 +0100

    header CSS

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.scss';
+import annonces from '../../images/Home-annonces.png';
+import animaux from '../../images/Home-animaux.png';
+import urgences from '../../images/Home-urgences.png';
 
 const Home = () => {
   return (
     <div className='home'>
       <div className='annonces'>
         <Link to='/annonces'>
+          <img className='image' alt='annonces' src={annonces} />
           <button className='button'>Les annonces</button>
         </Link>
       </div>
       <div className='animaux'>
         <Link to='/animaux'>
+          <img className='image' alt='animaux' src={animaux} />
           <button className='button'>Mes animaux</button>
         </Link>
       </div>
       <div className='urgences'>
         <Link to='/urgences'>
+          <img className='image' alt='urgences' src={urgences} />
           <button className='button'>Urgences</button>
         </Link>
       </div>
