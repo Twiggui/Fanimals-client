0a7ad125bfb1335dcd9edcb637f80a394656f70d
Guillem <guillem.dardill@gmail.com>
2021-02-03 11:29:47 +0100

    sign-in / sign-up CSS

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -21,9 +21,10 @@ const useStyles = makeStyles((theme) => ({
     borderRadius: 3,
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
     color: 'white',
-    height: 40,
+    height: 50,
     padding: '0 30px',
     margin: '5px 0px',
+    marginTop: '20px',
   },
 
   paper: {
